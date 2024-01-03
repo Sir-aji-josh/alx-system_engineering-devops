e7e7d7e15e54c238e93790222d79a373f07b518b
Sir-aji-josh <joshman.aji@gmail.com>
2024-01-03 07:27:14 -0800

    task 0.Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Python script that returns information using REST API
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
 from requests import get
