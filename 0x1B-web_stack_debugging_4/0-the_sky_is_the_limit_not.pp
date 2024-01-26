8d493dffcbced14275b166c31d25af9facb0a6cf
Sir-aji-josh <joshman.aji@gmail.com>
2024-01-25 16:30:15 -0800

    READMW.wqmd

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# increases the amount of traffic an Nginx server can handle.
+by increasing the maximum open file limit for the nginx user.
+ 
+# Increase the ULIMIT of the default file exec 
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+ 
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
+
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Enable the user Holberton to login and open files without error.
+#
+ Increase the hard file limit for Holberton users.exec { 'increase-hard-file-limit-for-holberton-user':  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',  path    => '/usr/local/bin/:/bin/'}
+# Increase the soft file limit for Holberton users.exec { 'increase-soft-file-limit-for-holberton-user':  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',  path    => '/usr/local/bin/:/bin/'
+}
